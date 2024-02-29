88d20fd5c0e64836f38b7e4a7a71402476060949
ashishjaiswal-nagarro <ashish.jaiswal@nagarro.com>
2024-02-29 09:12:44 +0530

    commit after preview enable for newBranch

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App"><header className="App-header"><h1>Hello1, World!</h1></header>
-   
-    
-    
+    <div className="App">
+      <header className="App-header">
+      <h1>Hello1, World!</h1></header>
+    <h1>Hello1, World!</h1>
+<h1>Hello1, World!</h1><h1>Hello1, World!</h1>
+    <h1>Hello1, World!</h1><h1>Hello1, World!</h1>
     
     </div>
   );
